af9e2a1d3f2569d9914d3ac2555d3a79e246447d
claremarie <cmmyers@gmail.com>
2019-05-19 16:34:17 -0700

    Updates

---
+++ precache-manifest.6c7b1d451713ff6834ca4a979f8412a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056d499cb3aa00cc2bc1f39b91daf73c",
+    "url": "/cmmyers/first-react-app/index.html"
+  },
+  {
+    "revision": "0f1959147b270a92d88f",
+    "url": "/cmmyers/first-react-app/static/css/main.d038b652.chunk.css"
+  },
+  {
+    "revision": "3f34d2ef590b2ed66219",
+    "url": "/cmmyers/first-react-app/static/js/2.ba7b6f9a.chunk.js"
+  },
+  {
+    "revision": "0f1959147b270a92d88f",
+    "url": "/cmmyers/first-react-app/static/js/main.536d49c9.chunk.js"
+  },
+  {
+    "revision": "db754d764a4a728805a3",
+    "url": "/cmmyers/first-react-app/static/js/runtime~main.6725856e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cmmyers/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
